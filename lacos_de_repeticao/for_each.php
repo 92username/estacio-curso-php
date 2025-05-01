eff6cea50876e988818a683d353d809b3bcba415
92username <115041664+92username@users.noreply.github.com>
2025-04-30 21:21:33 -0300

    feat: Add PHP files for repetition structures and practice examples

---
+++ lacos_de_repeticao/for_each.php
@@ -0,0 +1,14 @@
+<?php
+ $carros = Array("Fusca", "Monza", "Passat");
+
+ foreach($carros as $carro){
+	echo $carro;
+	echo "\n";
+ }
+
+// for normal
+
+ for ($i = 0; $i < count($carros); $i++) {
+	echo $carros[$i];
+	echo "\n";
+ }
